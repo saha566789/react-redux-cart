aaf865cad85fa40acb33f3cc27d41453b43d9c48
saha566789 <tomasaha3456@gmail.com>
2024-06-11 23:21:48 +0600

    add to cart page completed

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -21,14 +21,15 @@ const Cards = () => {
                         return (
                             <>
                                 <Card style={{ width: '22rem',border:"none" }} className='mx-2 mt-4 card_style'>
-                                    <Card.Img variant="top" src={element.imgdata} style={{height:"16rem"}} />
+                                    <Card.Img variant="top" src={element.imgdata} style={{height:"16rem"}} className='mt-4' />
                                     <Card.Body>
-                                        <Card.Title>Card Title</Card.Title>
+                                        <Card.Title>{element.rname}</Card.Title>
                                         <Card.Text>
-                                            Some quick example text to build on the card title and make up the
-                                            bulk of the card's content.
+                                           Price:${element.price}
                                         </Card.Text>
-                                        <Button variant="primary">Go somewhere</Button>
+                                       <div className='button_div d-flex justify-content-center align-content-center'>
+                                       <Button variant="primary" className='col-lg-12'>Add to Cart</Button>
+                                       </div>
                                     </Card.Body>
                                 </Card>
                             </>
